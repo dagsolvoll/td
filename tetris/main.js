84daced5c3e967275377892e6ec760dc35c546c9
Dag Solvoll <dag.solvoll@gmail.com>
2015-09-20 20:55:04 +0200

    added

--- tetris/main.js
+++ tetris/main.js
@@ -4,8 +4,7 @@ function log(debug, str) {
 }
 
 window.onload = function(e) {
-    log(0, "onload")
-    t = tetris("myCanvas", 192, 384)
+    t = tetris("myCanvas", 240, 480)
     t.canvas.focus()
 }
 
@@ -24,18 +23,27 @@ function tetris(id, w, h) {
         if (window === this) {
             return new tetris(id,w,h);
         }
-        this.canvas = document.getElementById(id);
-        this.canvas2 = document.getElementById(id + "2");
+        var tet = document.getElementById("tetris_game");
+        if (tet) {
+            var cnvs = document.createElement("CANVAS");
+            cnvs.id = "canvas1"
+            cnvs.width = w + 200
+            cnvs.height = h
+            cnvs.tabIndex="0"
+            tet.appendChild(cnvs)
+            this.canvas = cnvs
+        }
+
 
         this.ctx = this.canvas.getContext("2d");
 
-        this.cyanbrick   = {size: 4, blocks: [0x0F00, 0x2222, 0x00F0, 0x4444], color: 'cyan'   };
-        this.bluebrick   = {size: 3, blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20], color: 'blue'   };
-        this.orangebrick = {size: 3, blocks: [0x4460, 0x0E80, 0xC440, 0x2E00], color: 'orange' };
-        this.yellowbrick = {size: 2, blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00], color: 'yellow' };
-        this.greenbrick  = {size: 3, blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620], color: 'green'  };
-        this.purplebrick = {size: 3, blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640], color: 'purple' };
-        this.redbrick    = {size: 3, blocks: [0x0C60, 0x4C80, 0xC600, 0x2640], color: 'red'    };
+        this.cyanbrick   = {size: 4, blocks: [0x0F00, 0x2222, 0x00F0, 0x4444], color: 'cyan', points: 23   };
+        this.bluebrick   = {size: 3, blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20], color: 'blue', points: 21   };
+        this.orangebrick = {size: 3, blocks: [0x4460, 0x0E80, 0xC440, 0x2E00], color: 'orange', points: 25 };
+        this.yellowbrick = {size: 2, blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00], color: 'yellow', points: 22 };
+        this.greenbrick  = {size: 3, blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620], color: 'green', points: 26  };
+        this.purplebrick = {size: 3, blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640], color: 'purple', points: 28 };
+        this.redbrick    = {size: 3, blocks: [0x0C60, 0x4C80, 0xC600, 0x2640], color: 'red', points: 22    };
 
         this.pieces = []
 
@@ -57,7 +65,7 @@ tetris.prototype = {
         this.timer = null
         this.width = w
         this.height = h
-        this.block_size = 16
+        this.block_size = 24
         this.num_x = w / this.block_size
         this.num_y = h / this.block_size
         this.board = []
@@ -67,6 +75,9 @@ tetris.prototype = {
         this.STOP = 2
         this.ENDED = 3
         this.state = this.READY
+        this.level = 1
+        this.score = 0
+        this.lines = 0
 
         this.canvas.addEventListener("mousedown", function(event) {
             this.toggleMouse()
@@ -91,50 +102,63 @@ tetris.prototype = {
 //        return this.pieces[Math.round((this.pieces.length-1)*Math.random(0, this.pieces.length-1))];
     },
 
-    draw: function() {
-        this.ctx.clearRect(0,0,this.width,this.height);
-        this.drawlines()
-        this.drawbrick(this.ix, this.iy)
-        if (this.state != this.ENDED) {
-            log(0, this.ix + " - " + this.iy + " - " + (this.outside(this.ix, this.iy)))
-            this.iy += 1 
-            if (this.outside(this.ix, this.iy)) {
-                var br = this.brickcoord(this.ix, this.iy-1)
-                for (var i = 0; i < br.length; i++) {
-                    if (!this.board[br[i][1]])
-                        this.board[br[i][1]] = []
-                    this.board[br[i][1]][br[i][0]] = this.brick.color
-                }
-                this.ix = 3
-                this.iy = 0
-                this.brick = this.nextbrick
-                this.nextbrick = this.randombrick()
-            }
-            this.drawboard()
-            this.drawnextbrick()
+    dropone: function() {
+        if (this.iy <= 0)
+            this.iy += 1
+        else if (! this.outside(this.ix, this.iy + 1)) {
+            this.iy += 1
         } else {
+            var br = this.brickcoord(this.ix, this.iy)
+            for (var i = 0; i < br.length; i++) {
+                if (!this.board[br[i][1]])
+                    this.board[br[i][1]] = []
+                this.board[br[i][1]][br[i][0]] = this.brick.color
+            }
+            this.brick = this.nextbrick
+            this.nextbrick = this.randombrick()
+            this.iy = 0
+            this.ix = 3
+            this.score += this.brick.points
+        }
+    },
+
+    draw: function() {
+        this.dropone()
+        if (!this.outside(this.ix, this.iy))   {
+            this.ctx.clearRect(0,0,this.width,this.height);
+            this.drawlines()
+            this.drawbrick(this.ix, this.iy)
             this.drawboard()
-            this.drawnextbrick()        
-            this.toggleMouse()    
+            this.drawnextbrick()              
         }
     },
 
     drawnextbrick: function() {
-        ctx = this.canvas2.getContext("2d")
-        ctx.clearRect(0,0,64,64)
+        this.ctx.clearRect(this.width,0,this.block_size*8,this.block_size*12)
         var i = 0
         for (bit = 0x8000; bit > 0; bit = bit >> 1, i++) {
             row = Math.floor(i / 4)
             col = i - (row*4)
-            if (this.nextbrick.blocks[this.figidx] & bit) {
-                this.drawbox(ctx, this.nextbrick.color, col, row)
+            if (this.nextbrick.blocks[0] & bit) {
+                this.drawbox(this.ctx, this.nextbrick.color, this.width + (this.block_size*2), 0, col, row)
             }
         }
+        this.ctx.shadowBlur = 0;
+        this.ctx.fillStyle = "Black"
+        this.ctx.storeStyle = "Black"
+        this.ctx.font="20px Times Roman";
+        this.ctx.strokeRect(this.width + (this.block_size*0.5),this.block_size*4.2,150,this.block_size);
+        this.ctx.strokeRect(this.width + (this.block_size*0.5),this.block_size*6.2,150,this.block_size);
+        this.ctx.strokeRect(this.width + (this.block_size*0.5),this.block_size*8.2,150,this.block_size);
+        this.ctx.fillText("Level: " + this.level,this.width + (this.block_size*1),this.block_size*5);
+        this.ctx.fillText("Score: " + this.score,this.width + (this.block_size*1),this.block_size*7);
+        this.ctx.fillText("Lines: " + this.lines,this.width + (this.block_size*1),this.block_size*9);
+
     },
 
     drawlines: function() {
         this.ctx.strokeStyle = "#cccccc"
-        for (var i = 0; i < this.num_x; i++) {
+        for (var i = 0; i <= this.num_x; i++) {
             this.ctx.beginPath();
             this.ctx.moveTo(i*this.block_size,0);
             this.ctx.lineTo(i*this.block_size,this.num_y*this.block_size);
@@ -143,15 +167,17 @@ tetris.prototype = {
         for (var i = 0; i < this.num_y; i++) {
             this.ctx.beginPath();
             this.ctx.moveTo(0, i*this.block_size);
-            this.ctx.lineTo(this.num_y*this.block_size, i*this.block_size);
+            this.ctx.lineTo(this.num_x*this.block_size, i*this.block_size);
             this.ctx.stroke();
         }
     },
 
-    drawbox: function(ctx, color, x,y) {
+    drawbox: function(ctx, color, startX, startY, x,y) {
         var bs = this.block_size
         ctx.fillStyle = color;
-        ctx.fillRect (x*bs, y*bs, bs, bs);
+        ctx.shadowColor="black";
+        ctx.shadowBlur = 10;
+        ctx.fillRect (startX + (x*bs), startY + (y*bs), bs, bs);
     },
 
     drawboard: function(x, y){
@@ -180,6 +206,8 @@ tetris.prototype = {
                         else if (xi > i) 
                             nb[xi] = this.board[xi]
                     }
+                    this.lines += 1
+                    this.score += 100
                     this.board = nb
                 }
             }
@@ -199,7 +227,6 @@ tetris.prototype = {
         for (var i = 0; i < bc.length; i++) {
             var xx = bc[i][0]
             var xy = bc[i][1]
-            log(0, "O(" + i + "): " + xx + ", " + xy + " :: " + this.ix + " - " + this.iy )
             if (xy < 0 || xy >= this.num_y || xx < 0 || xx >= this.num_x) {
                 return true
             }
@@ -234,7 +261,6 @@ tetris.prototype = {
             if (this.brick.blocks[this.figidx] & bit) {
                 if (this.board[y+row] && this.board[y+row][x+col]) {
                     this.state = this.ENDED
-                    log(1, "Hit")
                     b = []
                     break
                 } else {
@@ -242,9 +268,7 @@ tetris.prototype = {
                 }
             }
         }
-        if (b.length > 0) {
-            b.forEach (function(a) {this.drawbox(this.ctx, this.brick.color, a[0], a[1])}.bind(this))
-        }
+        b.forEach (function(a) {this.drawbox(this.ctx, this.brick.color, 0, 0, a[0], a[1])}.bind(this))
     }, 
 
     rotate: function (forward) {        
@@ -268,7 +292,7 @@ tetris.prototype = {
                 clearInterval(this.timer) 
                 this.timer = null           
             } else {
-                this.timer = setInterval(this.draw.bind(this), 300)
+                this.timer = setInterval(this.draw.bind(this), 300*(2/this.level))
             }
         }
     }, 
